f144d62323436953ab941b7f51181f55340edcdd
JuanFTamayo <juantamayo237532@correo.itm.edu.co>
2021-08-26 22:21:10 -0500

    Fin

---
+++ week1.test/Test/ScheduledFunctionTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using week1.Functions.Functions;
+using week1.test.Helpers;
+using Xunit;
+
+namespace week1.test.Test
+{
+    public class ScheduledFunctionTest
+    {
+
+        [Fact]
+        public void ScheduledFunction_Should_Log_Message()
+        {
+            //Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+
+            //Act
+            ScheduledFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            //Asert
+            Assert.Contains("Deleting completed", message);
+        }
+    }
+}
